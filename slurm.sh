6956d5b081200888f49533d04f2e20d376ba9c1b
pkicki <>
2026-07-06 11:36:50 +0200

    Removed usunsed arguments

--- slurm.sh
+++ slurm.sh
@@ -14,16 +14,13 @@
 ##SBATCH --array=0-3
 
 eval "$(/mnt/storage_6/project_data/pl0467-01/soft/miniconda3/bin/conda shell.bash hook)"
-conda activate playground
+conda activate spectral
 
 SEED=${SLURM_ARRAY_TASK_ID}
 ENV_NAME=${ENV_NAME:-Go1JoystickFlatTerrain}
 IMPL=${IMPL:-jax}
 DOMAIN_RANDOMIZATION=${DOMAIN_RANDOMIZATION:-False}
 RESULTS_DIR=${RESULTS_DIR:-./results}
-WANDB_PROJECT=${WANDB_PROJECT:-}
-WANDB_ENTITY=${WANDB_ENTITY:-}
-WANDB_MODE=${WANDB_MODE:-online}
 
 echo "Running MuJoCo Playground PPO: ENV_NAME=${ENV_NAME}, IMPL=${IMPL}, DOMAIN_RANDOMIZATION=${DOMAIN_RANDOMIZATION}, SEED=${SEED}"
 
@@ -31,9 +28,5 @@ python train.py \
 	--env_name "${ENV_NAME}" \
 	--impl "${IMPL}" \
 	--domain_randomization "${DOMAIN_RANDOMIZATION}" \
-	--use_wandb True \
-	--wandb_project "${WANDB_PROJECT}" \
-	--wandb_entity "${WANDB_ENTITY}" \
-	--wandb_mode "${WANDB_MODE}" \
 	--results_dir "${RESULTS_DIR}" \
 	--seed $SEED
