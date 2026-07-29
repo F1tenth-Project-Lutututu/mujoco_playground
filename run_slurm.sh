4562bda6f37be96e14becfd4043dfc3b452f9957
pkicki <>
2026-07-29 18:26:03 +0200

    X

--- run_slurm.sh
+++ run_slurm.sh
@@ -22,15 +22,23 @@ export NUM_TIMESTEPS=400M
 #sbatch slurm.sh hp 3e-2 Op3Joystick 5.0 1.0
 #sbatch slurm.sh tr 2e-4 Go1JoystickFlatTerrain25 
 
-#for P in 1e-1 1e-2 1e-3 1e-4; do
+#for P in 1e-1 1e-2 1e-3 1e-4 1e-5; do
 #for P in 1e-2 1e-3 1e-4; do
 #for P in 1e0 1e1; do
-for P in 6e-4; do
+#for P in 6e-4; do
+#for P in 6e-3 6e-4; do
+#for P in 6e-5 6e-4; do
+#for P in 4e-3 6e-3; do
+#for P in 2e-3 6e-3; do
 #for P in 1e-5 8e-6 4e-6; do
 #for P in 2e-1 3e-1 1e0; do
 #for P in 2e-1 3e-1 5e-2; do
-#for P in 1e-5 2e-5 4e-5 6e-5 8e-5; do
+for P in 2e-5 4e-5 6e-5 8e-5; do
 #for P in 2e-4 4e-4 6e-4 8e-4; do
+#for P in 2e-3 4e-3 6e-3 8e-3; do
+#for P in 2e-2 4e-2 6e-2 8e-2; do
+#for P in 2e-1 4e-1 6e-1 8e-1; do
+#for P in 2e0 4e0 6e0 8e0; do
 #for P in 4e-4 5e-4 3e-4; do
 #for P in 2e-4 4e-4 8e-4; do
 #for P in 2e-3 4e-3 8e-3; do
@@ -59,7 +67,7 @@ for P in 6e-4; do
   #sbatch slurm.sh hp $P Go1JoystickFlatTerrain35 7.0 1.0
 
   #sbatch slurm.sh ar $P Go1JoystickFlatTerrain
-  sbatch slurm.sh tr $P Go1JoystickFlatTerrain 
+  #sbatch slurm.sh tr $P Go1JoystickFlatTerrain 
   #sbatch slurm.sh hp $P Go1JoystickFlatTerrain 5.0 1.0
 
   #sbatch slurm.sh ar $P Go1JoystickRoughTerrain
@@ -79,8 +87,8 @@ for P in 6e-4; do
   #sbatch slurm.sh hp $P SpotFlatTerrainJoystickPushesAndDomainRandomization 5.0 1.0
 
   #sbatch slurm.sh ar $P SpotJoystickGaitTracking
-  #sbatch slurm.sh tr $P SpotJoystickGaitTracking
-  #sbatch slurm.sh hp $P SpotJoystickGaitTracking 5.0 1.0
+  sbatch slurm.sh tr $P SpotJoystickGaitTracking
+  sbatch slurm.sh hp $P SpotJoystickGaitTracking 5.0 1.0
 
   #sbatch slurm.sh ar $P SpotJoystickGaitTrackingDomainRandomization
   #sbatch slurm.sh tr $P SpotJoystickGaitTrackingDomainRandomization
